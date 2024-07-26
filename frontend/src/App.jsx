4c5f7fe23f87f973d113ca31598ecad93444e7aa
Emmanuel-Adekunle <emmanuels.adekunle@gmail.com>
2024-07-26 07:03:44 +0100

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,35 @@
-// Importing necessary modules and components
-import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Navigation from "./pages/Auth/Navigation";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// App component
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      {/* ToastContainer to display toast notifications */}
-      <ToastContainer />
-      {/* Navigation component for the app's navigation bar */}
-      <Navigation />
-      <main className="py-3">
-        {/* Outlet component to render matched child routes */}
-        <Outlet />
-      </main>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
